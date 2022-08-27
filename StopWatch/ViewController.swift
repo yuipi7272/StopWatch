feebc8e996826e1a6333a04391beb2f8182dc3e6
YuiOgawa <yui20010218@gmail.com>
2022-08-27 14:53:42 +0900

    :sparkles: 教科書7完成

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @objc func up() {
+        // countを0.01足す
+        count = count + 0.01
+        // ラベルに小数点以下2桁まで表示
+        label.text = String(format: "%.2f", count)
+    }
+    
+    @IBAction func start() {
+        // タイマーが動作していなかったら、動かす
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    
+    @IBAction func stop() {
+        // タイマー動作していたら、停止する
+        if timer.isValid {
+            timer.invalidate()
+        }
+    }
 }
 
